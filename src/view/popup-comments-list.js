0e0bc922e517067bc093c983886d25c71b8ae2ea
daniyar.seferov <daniyar.seferov@rockdevels.com>
2021-04-01 18:07:13 +0300

    #Task1.3: View

--- src/view/popup-comments-list.js
+++ src/view/popup-comments-list.js
@@ -1,5 +1,5 @@
 export const createPopupCommentsListTemplate = (comments) => {
-  const listItems = comments.map((item) => { return `<li class="film-details__comment">${item}</li>`;}).join('');
+  const listItems = comments.map((item) => `<li class="film-details__comment">${item}</li>`).join('');
 
   return `<ul class="film-details__comments-list">
     ${listItems}
--- src/view/statistic-list.js
+++ src/view/statistic-list.js
@@ -1,5 +1,5 @@
 export const createStatisticListTemplate = (items) => {
-  const listItems = items.map((item) => { return `<li class="statistic__text-item">${item}</li>`;}).join('');
+  const listItems = items.map((item) => `<li class="statistic__text-item">${item}</li>`).join('');
 
   return `<ul class="statistic__text-list">
     ${listItems}
