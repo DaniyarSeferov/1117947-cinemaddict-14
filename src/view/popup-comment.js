8b4c617b199949f1fe1ab06fb094c8820db06b0f
daniyar.seferov <daniyar.seferov@rockdevels.com>
2021-06-01 17:38:33 +0300

    #Criterion 1.10: Disable Delete button

--- src/view/popup-comment.js
+++ src/view/popup-comment.js
@@ -15,7 +15,7 @@ const createPopupCommentTemplate = (data, state) => {
     <p class="film-details__comment-info">
       <span class="film-details__comment-author">${data.author ? data.author : ''}</span>
       <span class="film-details__comment-day">${date}</span>
-      <button class="film-details__comment-delete" data-id="${data.id}">${showDeleting ? 'Deleting...' : 'Delete'}</button>
+      <button class="film-details__comment-delete" data-id="${data.id}" ${isDeleting ? 'disabled' : ''}>${showDeleting ? 'Deleting...' : 'Delete'}</button>
     </p>
   </div>`;
 };
